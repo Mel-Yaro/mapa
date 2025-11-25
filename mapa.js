f8b593a743ba98b07e601fdfd848eb59921c19dc
Mel-Yaro <145498433+Mel-Yaro@users.noreply.github.com>
2025-11-25 18:42:50 +0000

    estilo de botones y textos y botones vínculados

--- mapa.js
+++ mapa.js
@@ -6,6 +6,20 @@ if (txMateria) {
   });
 }
 
+const txLomas = document.getElementById("tx-lomas");
+if (txMateria) {
+  txLomas.addEventListener("click", () => {
+    window.location.href = "textos.html?id=1";   
+  });
+}
+
+const txAsalto = document.getElementById("tx-asalto");
+if (txMateria) {
+  txAsalto.addEventListener("click", () => {
+    window.location.href = "textos.html?id=2";   
+  });
+}
+
 const cosasHuella = document.getElementById("cosas-huella");
 cosasHuella.addEventListener("click", (e) => {
 e.stopPropagation();
@@ -19,3 +33,18 @@ cosasHuella.addEventListener("mouseenter", () => {
   cosasHuella.addEventListener("mouseleave", () => {
     cosasHuella.style.opacity = "0.2";
   });
+
+  const btnInicio = document.getElementById('btn-inicio');
+const btnColecciones = document.getElementById('btn-colecciones');
+
+
+btnInicio.addEventListener("click", (e) => {
+e.stopPropagation();
+window.location.href = 'index.html';
+});
+
+
+btnColecciones.addEventListener("click", (e) => {
+e.stopPropagation();
+window.location.href = "coleccion.html";
+});
