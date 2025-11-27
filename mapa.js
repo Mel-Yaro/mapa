07514a35089858627ae8deab4ad93816e1ebf559
Mel-Yaro <145498433+Mel-Yaro@users.noreply.github.com>
2025-11-27 06:56:19 +0000

    Corrección de estilo en mapa y correcion de img en html

--- mapa.js
+++ mapa.js
@@ -27,237 +27,232 @@ const agua26 = document.getElementById("agua26"); if (agua26) agua26.addEventLis
 
 
 const soporte = document.getElementById("soporte");
-if (soporte) soporte.addEventListener("click", () => { window.location.href = "imgS.html?id=soporte"; });
+if (soporte) soporte.addEventListener("click", () => { window.location.href = "imgS.html?id=0"; });
 
 const soporte1 = document.getElementById("soporte1");
-if (soporte1) soporte1.addEventListener("click", () => { window.location.href = "imgS.html?id=soporte1"; });
+if (soporte1) soporte1.addEventListener("click", () => { window.location.href = "imgS.html?id=1"; });
 
 const soporte2 = document.getElementById("soporte2");
-if (soporte2) soporte2.addEventListener("click", () => { window.location.href = "imgS.html?id=soporte2"; });
+if (soporte2) soporte2.addEventListener("click", () => { window.location.href = "imgS.html?id=2"; });
 
 const soporte3 = document.getElementById("soporte3");
-if (soporte3) soporte3.addEventListener("click", () => { window.location.href = "imgS.html?id=soporte3"; });
+if (soporte3) soporte3.addEventListener("click", () => { window.location.href = "imgS.html?id=3"; });
 
 const soporte4 = document.getElementById("soporte4");
-if (soporte4) soporte4.addEventListener("click", () => { window.location.href = "imgS.html?id=soporte4"; });
+if (soporte4) soporte4.addEventListener("click", () => { window.location.href = "imgS.html?id=4"; });
 
 const soporte5 = document.getElementById("soporte5");
-if (soporte5) soporte5.addEventListener("click", () => { window.location.href = "imgS.html?id=soporte5"; });
+if (soporte5) soporte5.addEventListener("click", () => { window.location.href = "imgS.html?id=5"; });
 
 const soporte6 = document.getElementById("soporte6");
-if (soporte6) soporte6.addEventListener("click", () => { window.location.href = "imgS.html?id=soporte6"; });
+if (soporte6) soporte6.addEventListener("click", () => { window.location.href = "imgS.html?id=6"; });
 
 const soporte7 = document.getElementById("soporte7");
-if (soporte7) soporte7.addEventListener("click", () => { window.location.href = "imgS.html?id=soporte7"; });
+if (soporte7) soporte7.addEventListener("click", () => { window.location.href = "imgS.html?id=7"; });
 
 const soporte8 = document.getElementById("soporte8");
-if (soporte8) soporte8.addEventListener("click", () => { window.location.href = "imgS.html?id=soporte8"; });
+if (soporte8) soporte8.addEventListener("click", () => { window.location.href = "imgS.html?id=8"; });
 
 const soporte9 = document.getElementById("soporte9");
-if (soporte9) soporte9.addEventListener("click", () => { window.location.href = "imgS.html?id=soporte9"; });
+if (soporte9) soporte9.addEventListener("click", () => { window.location.href = "imgS.html?id=9"; });
 
 const soporte10 = document.getElementById("soporte10");
-if (soporte10) soporte10.addEventListener("click", () => { window.location.href = "imgS.html?id=soporte10"; });
+if (soporte10) soporte10.addEventListener("click", () => { window.location.href = "imgS.html?id=10"; });
 
 const soporte11 = document.getElementById("soporte11");
-if (soporte11) soporte11.addEventListener("click", () => { window.location.href = "imgS.html?id=soporte11"; });
+if (soporte11) soporte11.addEventListener("click", () => { window.location.href = "imgS.html?id=11"; });
 
 const soporte12 = document.getElementById("soporte12");
-if (soporte12) soporte12.addEventListener("click", () => { window.location.href = "imgS.html?id=soporte12"; });
+if (soporte12) soporte12.addEventListener("click", () => { window.location.href = "imgS.html?id=12"; });
 
 const soporte13 = document.getElementById("soporte13");
-if (soporte13) soporte13.addEventListener("click", () => { window.location.href = "imgS.html?id=soporte13"; });
+if (soporte13) soporte13.addEventListener("click", () => { window.location.href = "imgS.html?id=13"; });
 
 const soporte14 = document.getElementById("soporte14");
-if (soporte14) soporte14.addEventListener("click", () => { window.location.href = "imgS.html?id=soporte14"; });
+if (soporte14) soporte14.addEventListener("click", () => { window.location.href = "imgS.html?id=14"; });
 
 const soporte15 = document.getElementById("soporte15");
-if (soporte15) soporte15.addEventListener("click", () => { window.location.href = "imgS.html?id=soporte15"; });
+if (soporte15) soporte15.addEventListener("click", () => { window.location.href = "imgS.html?id=15"; });
 
 const huella = document.getElementById("huella");
-if (huella) huella.addEventListener("click", () => { window.location.href = "imgH.html?id=huella"; });
+if (huella) huella.addEventListener("click", () => { window.location.href = "imgH.html?id=0"; });
 
 const huella1 = document.getElementById("huella1");
-if (huella1) huella1.addEventListener("click", () => { window.location.href = "imgH.html?id=huella1"; });
+if (huella1) huella1.addEventListener("click", () => { window.location.href = "imgH.html?id=1"; });
 
 const huella2 = document.getElementById("huella2");
-if (huella2) huella2.addEventListener("click", () => { window.location.href = "imgH.html?id=huella2"; });
+if (huella2) huella2.addEventListener("click", () => { window.location.href = "imgH.html?id=2"; });
 
 const huella3 = document.getElementById("huella3");
-if (huella3) huella3.addEventListener("click", () => { window.location.href = "imgH.html?id=huella3"; });
+if (huella3) huella3.addEventListener("click", () => { window.location.href = "imgH.html?id=3"; });
 
 const huella4 = document.getElementById("huella4");
-if (huella4) huella4.addEventListener("click", () => { window.location.href = "imgH.html?id=huella4"; });
+if (huella4) huella4.addEventListener("click", () => { window.location.href = "imgH.html?id=4"; });
 
 const huella5 = document.getElementById("huella5");
-if (huella5) huella5.addEventListener("click", () => { window.location.href = "imgH.html?id=huella5"; });
+if (huella5) huella5.addEventListener("click", () => { window.location.href = "imgH.html?id=5"; });
 
 const huella6 = document.getElementById("huella6");
-if (huella6) huella6.addEventListener("click", () => { window.location.href = "imgH.html?id=huella6"; });
+if (huella6) huella6.addEventListener("click", () => { window.location.href = "imgH.html?id=6"; });
 
 const huella7 = document.getElementById("huella7");
-if (huella7) huella7.addEventListener("click", () => { window.location.href = "imgH.html?id=huella7"; });
+if (huella7) huella7.addEventListener("click", () => { window.location.href = "imgH.html?id=7"; });
 
 const huella8 = document.getElementById("huella8");
-if (huella8) huella8.addEventListener("click", () => { window.location.href = "imgH.html?id=huella8"; });
+if (huella8) huella8.addEventListener("click", () => { window.location.href = "imgH.html?id=8"; });
 
 const rama = document.getElementById("rama");
-if (rama) rama.addEventListener("click", () => { window.location.href = "imgM.html?id=rama"; });
+if (rama) rama.addEventListener("click", () => { window.location.href = "imgM.html?id=0"; });
 
 const rama1 = document.getElementById("rama1");
-if (rama1) rama1.addEventListener("click", () => { window.location.href = "imgM.html?id=rama1"; });
+if (rama1) rama1.addEventListener("click", () => { window.location.href = "imgM.html?id=1"; });
 
 const rama2 = document.getElementById("rama2");
-if (rama2) rama2.addEventListener("click", () => { window.location.href = "imgM.html?id=rama2"; });
+if (rama2) rama2.addEventListener("click", () => { window.location.href = "imgM.html?id=2"; });
 
 const rama3 = document.getElementById("rama3");
-if (rama3) rama3.addEventListener("click", () => { window.location.href = "imgM.html?id=rama3"; });
+if (rama3) rama3.addEventListener("click", () => { window.location.href = "imgM.html?id=3"; });
 
 const rama4 = document.getElementById("rama4");
-if (rama4) rama4.addEventListener("click", () => { window.location.href = "imgM.html?id=rama4"; });
+if (rama4) rama4.addEventListener("click", () => { window.location.href = "imgM.html?id=4"; });
 
 const rama5 = document.getElementById("rama5");
-if (rama5) rama5.addEventListener("click", () => { window.location.href = "imgM.html?id=rama5"; });
+if (rama5) rama5.addEventListener("click", () => { window.location.href = "imgM.html?id=5"; });
 
 const rama6 = document.getElementById("rama6");
-if (rama6) rama6.addEventListener("click", () => { window.location.href = "imgM.html?id=rama6"; });
+if (rama6) rama6.addEventListener("click", () => { window.location.href = "imgM.html?id=6"; });
 
 const rama7 = document.getElementById("rama7");
-if (rama7) rama7.addEventListener("click", () => { window.location.href = "imgM.html?id=rama7"; });
+if (rama7) rama7.addEventListener("click", () => { window.location.href = "imgM.html?id=7"; });
 
 const rama8 = document.getElementById("rama8");
-if (rama8) rama8.addEventListener("click", () => { window.location.href = "imgM.html?id=rama8"; });
+if (rama8) rama8.addEventListener("click", () => { window.location.href = "imgM.html?id=8"; });
 
 const rama9 = document.getElementById("rama9");
-if (rama9) rama9.addEventListener("click", () => { window.location.href = "imgM.html?id=rama9"; });
+if (rama9) rama9.addEventListener("click", () => { window.location.href = "imgM.html?id=9"; });
 
 const rama10 = document.getElementById("rama10");
-if (rama10) rama10.addEventListener("click", () => { window.location.href = "imgM.html?id=rama10"; });
+if (rama10) rama10.addEventListener("click", () => { window.location.href = "imgM.html?id=10"; });
 
 const rama11 = document.getElementById("rama11");
-if (rama11) rama11.addEventListener("click", () => { window.location.href = "imgM.html?id=rama11"; });
+if (rama11) rama11.addEventListener("click", () => { window.location.href = "imgM.html?id=11"; });
 
 const rama12 = document.getElementById("rama12");
-if (rama12) rama12.addEventListener("click", () => { window.location.href = "imgM.html?id=rama12"; });
+if (rama12) rama12.addEventListener("click", () => { window.location.href = "imgM.html?id=12"; });
 
 const rama13 = document.getElementById("rama13");
-if (rama13) rama13.addEventListener("click", () => { window.location.href = "imgM.html?id=rama13"; });
+if (rama13) rama13.addEventListener("click", () => { window.location.href = "imgM.html?id=13"; });
 
 const rama14 = document.getElementById("rama14");
-if (rama14) rama14.addEventListener("click", () => { window.location.href = "imgM.html?id=rama14"; });
+if (rama14) rama14.addEventListener("click", () => { window.location.href = "imgM.html?id=14"; });
 
 const rama15 = document.getElementById("rama15");
-if (rama15) rama15.addEventListener("click", () => { window.location.href = "imgM.html?id=rama15"; });
+if (rama15) rama15.addEventListener("click", () => { window.location.href = "imgM.html?id=15"; });
 
 const rama16 = document.getElementById("rama16");
-if (rama16) rama16.addEventListener("click", () => { window.location.href = "imgM.html?id=rama16"; });
+if (rama16) rama16.addEventListener("click", () => { window.location.href = "imgM.html?id=16"; });
 
 const rama17 = document.getElementById("rama17");
-if (rama17) rama17.addEventListener("click", () => { window.location.href = "imgM.html?id=rama17"; });
-
+if (rama17) rama17.addEventListener("click", () => { window.location.href = "imgM.html?id=17"; });
 
 const puente = document.getElementById("puente");
-if (puente) puente.addEventListener("click", () => { window.location.href = "imgP.html?id=puente"; });
+if (puente) puente.addEventListener("click", () => { window.location.href = "imgP.html?id=0"; });
 
 const puente1 = document.getElementById("puente1");
-if (puente1) puente1.addEventListener("click", () => { window.location.href = "imgP.html?id=puente1"; });
+if (puente1) puente1.addEventListener("click", () => { window.location.href = "imgP.html?id=1"; });
 
 const puente2 = document.getElementById("puente2");
-if (puente2) puente2.addEventListener("click", () => { window.location.href = "imgP.html?id=puente2"; });
+if (puente2) puente2.addEventListener("click", () => { window.location.href = "imgP.html?id=2"; });
 
 const puente3 = document.getElementById("puente3");
-if (puente3) puente3.addEventListener("click", () => { window.location.href = "imgP.html?id=puente3"; });
+if (puente3) puente3.addEventListener("click", () => { window.location.href = "imgP.html?id=3"; });
 
 const puente4 = document.getElementById("puente4");
-if (puente4) puente4.addEventListener("click", () => { window.location.href = "imgP.html?id=puente4"; });
+if (puente4) puente4.addEventListener("click", () => { window.location.href = "imgP.html?id=4"; });
 
 const puente5 = document.getElementById("puente5");
-if (puente5) puente5.addEventListener("click", () => { window.location.href = "imgP.html?id=puente5"; });
+if (puente5) puente5.addEventListener("click", () => { window.location.href = "imgP.html?id=5"; });
 
 const puente6 = document.getElementById("puente6");
-if (puente6) puente6.addEventListener("click", () => { window.location.href = "imgP.html?id=puente6"; });
+if (puente6) puente6.addEventListener("click", () => { window.location.href = "imgP.html?id=6"; });
 
 const puente7 = document.getElementById("puente7");
-if (puente7) puente7.addEventListener("click", () => { window.location.href = "imgP.html?id=puente7"; });
+if (puente7) puente7.addEventListener("click", () => { window.location.href = "imgP.html?id=7"; });
 
 const puente8 = document.getElementById("puente8");
-if (puente8) puente8.addEventListener("click", () => { window.location.href = "imgP.html?id=puente8"; });
+if (puente8) puente8.addEventListener("click", () => { window.location.href = "imgP.html?id=8"; });
 
 const puente9 = document.getElementById("puente9");
-if (puente9) puente9.addEventListener("click", () => { window.location.href = "imgP.html?id=puente9"; });
+if (puente9) puente9.addEventListener("click", () => { window.location.href = "imgP.html?id=9"; });
 
 const puente10 = document.getElementById("puente10");
-if (puente10) puente10.addEventListener("click", () => { window.location.href = "imgP.html?id=puente10"; });
+if (puente10) puente10.addEventListener("click", () => { window.location.href = "imgP.html?id=10"; });
 
 const puerta = document.getElementById("puerta");
-if (puerta) puerta.addEventListener("click", () => { window.location.href = "imgPU.html?id=puerta"; });
+if (puerta) puerta.addEventListener("click", () => { window.location.href = "imgPU.html?id=0"; });
 
 const puerta1 = document.getElementById("puerta1");
-if (puerta1) puerta1.addEventListener("click", () => { window.location.href = "imgPU.html?id=puerta1"; });
+if (puerta1) puerta1.addEventListener("click", () => { window.location.href = "imgPU.html?id=1"; });
 
 const puerta2 = document.getElementById("puerta2");
-if (puerta2) puerta2.addEventListener("click", () => { window.location.href = "imgPU.html?id=puerta2"; });
+if (puerta2) puerta2.addEventListener("click", () => { window.location.href = "imgPU.html?id=2"; });
 
 const puerta3 = document.getElementById("puerta3");
-if (puerta3) puerta3.addEventListener("click", () => { window.location.href = "imgPU.html?id=puerta3"; });
+if (puerta3) puerta3.addEventListener("click", () => { window.location.href = "imgPU.html?id=3"; });
 
 const puerta4 = document.getElementById("puerta4");
-if (puerta4) puerta4.addEventListener("click", () => { window.location.href = "imgPU.html?id=puerta4"; });
+if (puerta4) puerta4.addEventListener("click", () => { window.location.href = "imgPU.html?id=4"; });
 
 const puerta5 = document.getElementById("puerta5");
-if (puerta5) puerta5.addEventListener("click", () => { window.location.href = "imgPU.html?id=puerta5"; });
+if (puerta5) puerta5.addEventListener("click", () => { window.location.href = "imgPU.html?id=5"; });
 
 const puerta6 = document.getElementById("puerta6");
-if (puerta6) puerta6.addEventListener("click", () => { window.location.href = "imgPU.html?id=puerta6"; });
+if (puerta6) puerta6.addEventListener("click", () => { window.location.href = "imgPU.html?id=6"; });
 
 const puerta7 = document.getElementById("puerta7");
-if (puerta7) puerta7.addEventListener("click", () => { window.location.href = "imgPU.html?id=puerta7"; });
+if (puerta7) puerta7.addEventListener("click", () => { window.location.href = "imgPU.html?id=7"; });
 
 const puerta8 = document.getElementById("puerta8");
-if (puerta8) puerta8.addEventListener("click", () => { window.location.href = "imgPU.html?id=puerta8"; });
+if (puerta8) puerta8.addEventListener("click", () => { window.location.href = "imgPU.html?id=8"; });
 
 const puerta9 = document.getElementById("puerta9");
-if (puerta9) puerta9.addEventListener("click", () => { window.location.href = "imgPU.html?id=puerta9"; });
+if (puerta9) puerta9.addEventListener("click", () => { window.location.href = "imgPU.html?id=9"; });
 
 const ruina = document.getElementById("ruina");
-if (ruina) ruina.addEventListener("click", () => { window.location.href = "imgR.html?id=ruina"; });
+if (ruina) ruina.addEventListener("click", () => { window.location.href = "imgR.html?id=0"; });
 
 const ruina1 = document.getElementById("ruina1");
-if (ruina1) ruina1.addEventListener("click", () => { window.location.href = "imgR.html?id=ruina1"; });
+if (ruina1) ruina1.addEventListener("click", () => { window.location.href = "imgR.html?id=1"; });
 
 const ruina2 = document.getElementById("ruina2");
-if (ruina2) ruina2.addEventListener("click", () => { window.location.href = "imgR.html?id=ruina2"; });
+if (ruina2) ruina2.addEventListener("click", () => { window.location.href = "imgR.html?id=2"; });
 
 const ruina3 = document.getElementById("ruina3");
-if (ruina3) ruina3.addEventListener("click", () => { window.location.href = "imgR.html?id=ruina3"; });
+if (ruina3) ruina3.addEventListener("click", () => { window.location.href = "imgR.html?id=3"; });
 
 const ruina4 = document.getElementById("ruina4");
-if (ruina4) ruina4.addEventListener("click", () => { window.location.href = "imgR.html?id=ruina4"; });
+if (ruina4) ruina4.addEventListener("click", () => { window.location.href = "imgR.html?id=4"; });
 
 const ruina5 = document.getElementById("ruina5");
-if (ruina5) ruina5.addEventListener("click", () => { window.location.href = "imgR.html?id=ruina5"; });
+if (ruina5) ruina5.addEventListener("click", () => { window.location.href = "imgR.html?id=5"; });
 
 const ruina6 = document.getElementById("ruina6");
-if (ruina6) ruina6.addEventListener("click", () => { window.location.href = "imgR.html?id=ruina6"; });
+if (ruina6) ruina6.addEventListener("click", () => { window.location.href = "imgR.html?id=6"; });
 
 const ruina7 = document.getElementById("ruina7");
-if (ruina7) ruina7.addEventListener("click", () => { window.location.href = "imgR.html?id=ruina7"; });
+if (ruina7) ruina7.addEventListener("click", () => { window.location.href = "imgR.html?id=7"; });
 
 const ruina8 = document.getElementById("ruina8");
-if (ruina8) ruina8.addEventListener("click", () => { window.location.href = "imgR.html?id=ruina8"; });
+if (ruina8) ruina8.addEventListener("click", () => { window.location.href = "imgR.html?id=8"; });
 
 const ruina9 = document.getElementById("ruina9");
-if (ruina9) ruina9.addEventListener("click", () => { window.location.href = "imgR.html?id=ruina9"; });
+if (ruina9) ruina9.addEventListener("click", () => { window.location.href = "imgR.html?id=9"; });
 
 const ruina10 = document.getElementById("ruina10");
-if (ruina10) ruina10.addEventListener("click", () => { window.location.href = "imgR.html?id=ruina10"; });
+if (ruina10) ruina10.addEventListener("click", () => { window.location.href = "imgR.html?id=10"; });
 
 const ruina11 = document.getElementById("ruina11");
-if (ruina11) ruina11.addEventListener("click", () => { window.location.href = "imgR.html?id=ruina11"; });
-
-
-
-
+if (ruina11) ruina11.addEventListener("click", () => { window.location.href = "imgR.html?id=11"; });
 
 
 
